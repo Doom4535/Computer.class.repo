a523b97304ec94d44806a2aaecbebbd167a87d8f
Aaron Covrig <Aaron@Dominion.local>
2014-01-22 18:03:42 -0500

    Added level 318 Data Structures and Algorithms to the repository

--- CPTR.124.git.repo.uploader.sh
+++ CPTR.124.git.repo.uploader.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Download the code and extract it.
+# Now includes both the 124 and 318 level programming classes.
+# Download the code and extract it. 
 mkdir Temp.Cache
 curl http://www.cs.southern.edu/halterman/repository/ClassCode/124/CPTR_124_Class_Code.zip -o Temp.Cache/CPTR_124_Class_Code.zip
+curl http://www.cs.southern.edu/halterman/repository/ClassCode/318/CPTR_318_Class_Code.zip -o Temp.Cache/CPTR_318_Class_Code.zip
 unzip -uo Temp.Cache/*.zip
 rm -vr Temp.Cache
 
